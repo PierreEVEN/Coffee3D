600ffcb1f57693dd3108597a2d9d5eec71874889
Pierre EVEN <evenpierre@orange.fr>
2020-11-14 19:26:22 +0100

    moved engine version to 1.1.0

--- src/coffee3D/core/Engine.java
+++ src/coffee3D/core/Engine.java
@@ -2,8 +2,8 @@ package coffee3D.core;
 
 public class Engine {
     public static final int ENGINE_PRIMARY_VERSION = 1;
-    public static final int ENGINE_SECONDARY_VERSION = 0;
-    public static final int ENGINE_RELEASE_NUMBER = 1;
+    public static final int ENGINE_SECONDARY_VERSION = 1;
+    public static final int ENGINE_RELEASE_NUMBER = 0;
 
     public static String GetEngineVersion() { return ENGINE_PRIMARY_VERSION + "." + ENGINE_SECONDARY_VERSION + "." + ENGINE_RELEASE_NUMBER; }
 }
